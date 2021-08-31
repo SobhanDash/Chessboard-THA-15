e9ee06b47b0a44a19c64b0b8e0e5df0e4d325bfd
Sobhan Dash <52308005+SobhanDash@users.noreply.github.com>
2021-08-31 11:32:54 +0530

    another chessboard using react props and conditional rendering

--- src/App.js
+++ src/App.js
@@ -4,51 +4,50 @@ function Cell() {
   return <div className="cell"></div>;
 }
 
+function Box({ cid, alter }) {
+  if (alter) {
+    return (
+      <div
+        className="box"
+        style={{ background: cid % 2 === 0 ? "black" : "white" }}
+      ></div>
+    );
+  } else {
+    return (
+      <div
+        className="box"
+        style={{ background: cid % 2 === 0 ? "white" : "black" }}
+      ></div>
+    );
+  }
+}
+
 export default function App() {
+  let alter = false;
   return (
     <div className="App">
-      <div className="main">
-        {[...Array(64)].map(() => {
-          return <Cell />;
-        })}
+      <div className="chessboards">
+        <div className="first">
+          <h1>Using nth child selector</h1>
+          <div className="board">
+            {[...Array(64)].map((index) => {
+              return <Cell key={index} />;
+            })}
+          </div>
+        </div>
+
+        <div className="second">
+          <h1>Using React Props</h1>
+          <div className="board2">
+            {[...Array(64)].map((box, cid) => {
+              if (cid % 8 === 0 && cid !== 0) {
+                alter = !alter;
+              }
+              return <Box cid={cid} alter={alter} />;
+            })}
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-// function Cell({ i, alter }) {
-//   // console.log(i, alter);
-
-//   if (alter) {
-//     return (
-//       <div
-//         className="cell"
-//         style={{ background: i % 2 === 0 ? "black" : "white" }}
-//       ></div>
-//     );
-//   } else {
-//     return (
-//       <div
-//         className="cell"
-//         style={{ background: i % 2 === 0 ? "white" : "black" }}
-//       ></div>
-//     );
-//   }
-// }
-
-//export default function App() {
-//   let alter = false;
-//   return (
-//     <div className="App">
-//       <div className="main">
-//         {[...Array(64)].map((cell, i) => {
-//           // console.log(i%8)
-//           if (i % 8 === 0 && i !== 0) {
-//             alter = !alter;
-//           }
-//           return <Cell i={i} alter={alter} />;
-//         })}
-//       </div>
-//     </div>
-//   );
-// }
